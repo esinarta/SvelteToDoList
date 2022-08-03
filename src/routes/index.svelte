7223e4ec8e98b6daedeabff78a2fffea4f29a085
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2022-08-03 16:42:24 -0700

    Set up starter app and create basic non-persistent to-do list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let newItemTitle = '';
+
+  let list = [
+    {
+      title: 'Item 1',
+      done: false
+    },
+    {
+      title: 'Item 2',
+      done: false
+    },
+    {
+      title: 'Item 3',
+      done: true
+
+    }
+  ]
+
+  const addItem = () => {
+    list = [...list, {title: newItemTitle, done: false}];
+    newItemTitle = '';
+  }
+
+  const removeItem = (index: number) => {
+    list.splice(index, 1)
+		list = list;
+  }
+</script>
+
+<h1>To-Do List</h1>
+
+<div>
+  <input bind:value={newItemTitle} type="text" placeholder="New item" />
+  <button on:click={addItem}>Add</button>
+</div>
+
+<div>
+  {#each list as listItem, index}
+    <input bind:checked={listItem.done} type="checkbox">
+    <span>{listItem.title}</span>
+    <button on:click={() => removeItem(index)}>Remove</button>
+    <br/>
+  {/each} 
+</div>
