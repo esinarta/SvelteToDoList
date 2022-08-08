730b3671b9db622d2f881c0550e33b31e7ec0736
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2022-08-08 15:48:17 -0700

    Add routes for Home and About pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,7 @@
-<script lang="ts">
-  let newItemTitle = '';
-
-  let list = [
-    {
-      title: 'Item 1',
-      done: false
-    },
-    {
-      title: 'Item 2',
-      done: false
-    },
-    {
-      title: 'Item 3',
-      done: true
-
-    }
-  ]
-
-  const addItem = () => {
-    list = [...list, {title: newItemTitle, done: false}];
-    newItemTitle = '';
-  }
-
-  const removeItem = (index: number) => {
-    list.splice(index, 1)
-		list = list;
-  }
-</script>
-
-<h1>To-Do List</h1>
-
 <div>
-  <input bind:value={newItemTitle} type="text" placeholder="New item" />
-  <button on:click={addItem}>Add</button>
-</div>
-
-<div>
-  {#each list as listItem, index}
-    <input bind:checked={listItem.done} type="checkbox">
-    <span>{listItem.title}</span>
-    <button on:click={() => removeItem(index)}>Remove</button>
-    <br/>
-  {/each} 
+  <h1>Home</h1>
+  <ul>
+    <li><a href="/about">About</a></li>
+    <li><a href="/todo">To-Do</a></li>
+  </ul>
 </div>
