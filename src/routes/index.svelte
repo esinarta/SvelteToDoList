1f55c18d65bb71e98099e158fa12a713c7570046
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2022-08-16 16:48:27 -0700

    Add auth with magic email link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+  import { user } from '../lib/sessionStore';
+  import { supabase } from '../lib/supabaseClient';
+  import Auth from '../lib/Auth.svelte';
+
+  user.set(!!supabase.auth.user());
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    user.set(!!session?.user);
+  });
+</script>
+
 <div>
+  {#if $user}
   <h1>Home</h1>
   <ul>
     <li><a href="/about">About</a></li>
     <li><a href="/todo">To-Do</a></li>
   </ul>
+  {:else}
+  <Auth />
+  {/if}
 </div>
