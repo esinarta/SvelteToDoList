076983b91a030092f11caa38d14e5820096217eb
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2022-08-22 15:57:00 -0700

    Add error messages to login and signup form

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -4,28 +4,28 @@
   let loading = false;
   let email = '';
   let password = '';
+  let message = '';
 
   const handleAuth = async (type: string) => {
-    try {
-      loading = true;
-      const { user, error } = 
-        type ==='login' 
-          ? await supabase.auth.signIn({ email, password }) 
-          : await supabase.auth.signUp({ email, password });
+    loading = true;
+    const { user, error } = 
+      type ==='login' 
+        ? await supabase.auth.signIn({ email, password }) 
+        : await supabase.auth.signUp({ email, password });
 
-      if (error) throw error;
-      else if (!user && !error) alert('A link has been sent to your email.');
-    } catch (error) {
-      if (error instanceof Error) alert(error.message);
-    } finally {
-      loading = false;
-    }
-  };
+    if (error) {
+      message = error.message
+    } else {
+      message = 'A link has been sent to your email.';
+    } 
+    
+    loading = false;
+  }
 </script>
 
-<form>
-  <div>
-    <h1>To-Do App</h1>
+<div>
+  <h1>To-Do App</h1>
+    <form>
     <p>Log in or sign up:</p>
     <div>
       <input
@@ -39,6 +39,11 @@
         bind:value="{password}"
       />
     </div>
+    {#if message}
+      <div>
+        {message}
+      </div>
+    {/if}
     <div>
       <input type="submit" value={loading ? "Loading" :
       "Log In"} disabled={loading} on:click={() => handleAuth('login')}/>
@@ -46,5 +51,5 @@
       <input type="submit" value={loading ? "Loading" :
       "Sign Up"} disabled={loading} on:click={() => handleAuth('signup')}/>
     </div>
-  </div>
-</form>
+  </form>
+</div>
